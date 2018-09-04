3acc60f6eb6f43878dd8c950c74386127228db2c
Peter Hutta <PeterH@kentico.com>
2018-09-04 09:22:31 +0200

    KA-366 Install NUnit framework

--- TodoApp/TodoApp.Api/Properties/AssemblyInfo.cs
+++ TodoApp/TodoApp.Api/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TodoApp.Api")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Web API provides CRUD operations for https://github.com/pthutta/kentico-onboarding-js")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TodoApp.Api")]
