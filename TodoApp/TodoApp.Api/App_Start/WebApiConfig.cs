3cd442b257169389fb18ec0de2a543b87fe85019
Peter Hutta <PeterH@kentico.com>
2018-09-05 13:51:59 +0200

    KA-367 Set-up URL versioning in ItemsController

--- TodoApp/TodoApp.Api/App_Start/WebApiConfig.cs
+++ TodoApp/TodoApp.Api/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Routing;
+using Microsoft.Web.Http.Routing;
 
 namespace TodoApp.Api
 {
@@ -10,13 +12,21 @@ namespace TodoApp.Api
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var constraintResolver = new DefaultInlineConstraintResolver()
+            {
+                ConstraintMap =
+                {
+                    ["apiVersion"] = typeof( ApiVersionRouteConstraint )
+                }
+            };
 
             // Web API routes
-            config.MapHttpAttributeRoutes();
+            config.MapHttpAttributeRoutes(constraintResolver);
+            config.AddApiVersioning();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/v{version:apiVersion}/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
--- TodoApp/TodoApp.Api/Controllers/ItemsController.cs
+++ TodoApp/TodoApp.Api/Controllers/ItemsController.cs
@@ -5,10 +5,13 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
+using Microsoft.Web.Http;
 using TodoApp.Api.Models;
 
 namespace TodoApp.Api.Controllers
 {
+    [ApiVersion("1.0")]
+    [RoutePrefix("api/v{version:apiVersion}/Items")]
     public class ItemsController : ApiController
     {
         private static readonly IList<Item> Items = new List<Item>();
@@ -34,28 +37,32 @@ namespace TodoApp.Api.Controllers
             });
         }
 
-        // GET: api/Items
+        // GET: api/v1/Items
+        [Route]
         public IEnumerable<Item> Get()
         {
             return Items;
         }
 
-        // GET: api/Items/5
+        // GET: api/v1/Items/5
+        [Route("{id}")]
         [ResponseType(typeof(Item))]
         public IHttpActionResult Get(string id)
         {
             return Ok(Items.FirstOrDefault(i => i.Id == id));
         }
 
-        // POST: api/Items
+        // POST: api/v1/Items
+        [Route("", Name = "PostNewItem")]
         [ResponseType(typeof(Item))]
         public IHttpActionResult Post([FromBody]Item item)
         {
             Items.Add(item);
-            return CreatedAtRoute("DefaultApi", new { id = item.Id }, item);
+            return CreatedAtRoute("PostNewItem", new { id = item.Id }, item);
         }
 
-        // PUT: api/Items/5
+        // PUT: api/v1/Items/5
+        [Route("{id}")]
         [ResponseType(typeof(void))]
         public IHttpActionResult Put(string id, [FromBody]Item value)
         {
@@ -67,7 +74,8 @@ namespace TodoApp.Api.Controllers
             return StatusCode(HttpStatusCode.NoContent);
         }
 
-        // DELETE: api/Items/5
+        // DELETE: api/v1/Items/5
+        [Route("{id}")]
         [ResponseType(typeof(Item))]
         public IHttpActionResult Delete(string id)
         {
