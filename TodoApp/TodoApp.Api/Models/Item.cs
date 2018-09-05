050151c4425504b197bc7e6027e9ed50cf297d47
Peter Hutta <PeterH@kentico.com>
2018-09-05 09:53:16 +0200

    KA-367 Add Item model

---
+++ TodoApp/TodoApp.Api/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TodoApp.Api.Models
+{
+    public class Item
+    {
+        [Key]
+        public string Id { get; set; }
+        [MinLength(1, ErrorMessage = "Text length must be greater than 1.")]
+        public string Text { get; set; }
+    }
+}
