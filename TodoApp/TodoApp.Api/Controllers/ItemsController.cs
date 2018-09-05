1e25c4135e377e02475d2cd39a269d36a2b7a4f2
Peter Hutta <PeterH@kentico.com>
2018-09-05 10:53:52 +0200

    KA-367 Implement actions in ItemsController

--- TodoApp/TodoApp.Api/Controllers/ItemsController.cs
+++ TodoApp/TodoApp.Api/Controllers/ItemsController.cs
@@ -4,36 +4,79 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
+using TodoApp.Api.Models;
 
 namespace TodoApp.Api.Controllers
 {
     public class ItemsController : ApiController
     {
-        // GET: api/Items
-        public IEnumerable<string> Get()
+        private static readonly IList<Item> Items = new List<Item>();
+
+        public ItemsController()
         {
-            return new string[] { "value1", "value2" };
+            if (Items.Count != 0) return;
+
+            Items.Add(new Item
+            {
+                Id = "1",
+                Text = "Learn react"
+            });
+            Items.Add(new Item
+            {
+                Id = "2",
+                Text = "Learn redux"
+            });
+            Items.Add(new Item
+            {
+                Id = "3",
+                Text = "Write Web API"
+            });
+        }
+
+        // GET: api/Items
+        public IEnumerable<Item> Get()
+        {
+            return Items;
         }
 
         // GET: api/Items/5
-        public string Get(int id)
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult Get(string id)
         {
-            return "value";
+            return Ok(Items.FirstOrDefault(i => i.Id == id));
         }
 
         // POST: api/Items
-        public void Post([FromBody]string value)
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult Post([FromBody]Item item)
         {
+            Items.Add(item);
+            return CreatedAtRoute("DefaultApi", new { id = item.Id }, item);
         }
 
         // PUT: api/Items/5
-        public void Put(int id, [FromBody]string value)
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Put(string id, [FromBody]Item value)
         {
+            var item = Items.FirstOrDefault(i => i.Id == id);
+            if (item != null)
+            {
+                item.Text = value.Text;
+            }
+            return StatusCode(HttpStatusCode.NoContent);
         }
 
         // DELETE: api/Items/5
-        public void Delete(int id)
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult Delete(string id)
         {
+            var item = Items.FirstOrDefault(i => i.Id == id);
+            if (item != null)
+            {
+                Items.Remove(item);
+            }
+            return Ok(item);
         }
     }
 }
