e4e8a6d3c19137d2534f8838739f4c3b56ba2a1f
Peter Hutta <PeterH@kentico.com>
2018-09-06 15:47:10 +0200

    KA-367 Configure Json formatter to return json responses in browsers

--- TodoApp/TodoApp.Api/App_Start/WebApiConfig.cs
+++ TodoApp/TodoApp.Api/App_Start/WebApiConfig.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using System.Web.Http.Routing;
 using Microsoft.Web.Http.Routing;
+using Newtonsoft.Json.Serialization;
 
 namespace TodoApp.Api
 {
@@ -23,6 +26,13 @@ namespace TodoApp.Api
             // Web API routes
             config.MapHttpAttributeRoutes(constraintResolver);
             config.AddApiVersioning();
+            
+            // Configure Json Formatter
+            JsonMediaTypeFormatter jsonFormatter = config.Formatters.JsonFormatter;
+            jsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            jsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/x-www-form-urlencoded"));
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
         }
     }
 }
