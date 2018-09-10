3d0967a22b54326b185ef02b5bb8269d46814e21
Peter Hutta <PeterH@kentico.com>
2018-09-10 15:17:35 +0200

    KA-368 Add IItemRepository interface and implement it

---
+++ TodoApp/TodoApp.DAL/Repositories/IItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoApp.DAL.Entities;
+
+namespace TodoApp.DAL.Repositories
+{
+    public interface IItemRepository
+    {
+        Task<IEnumerable<Item>> GetAllAsync();
+
+        Task<Item> GetByIdAsync(string id);
+
+        Task CreateAsync(Item item);
+
+        Task DeleteAsync(Item item);
+
+        Task UpdateAsync(Item item);
+    }
+}
---
+++ TodoApp/TodoApp.DAL/Repositories/ItemRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.DAL.Entities;
+
+namespace TodoApp.DAL.Repositories
+{
+    public class ItemRepository : IItemRepository
+    {
+        public async Task<IEnumerable<Item>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Item> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task CreateAsync(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
