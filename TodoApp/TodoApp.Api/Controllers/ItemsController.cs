3c3f2faf992c625e7ff240786a54c8ba71b5e1b2
Peter Hutta <PeterH@kentico.com>
2018-09-10 15:56:00 +0200

    KA-368 Implement actions in ItemsController

--- TodoApp/TodoApp.Api/Controllers/ItemsController.cs
+++ TodoApp/TodoApp.Api/Controllers/ItemsController.cs
@@ -2,6 +2,8 @@
 using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.Web.Http;
+using TodoApp.DAL.Entities;
+using TodoApp.DAL.Repositories;
 
 namespace TodoApp.Api.Controllers
 {
@@ -9,54 +11,50 @@ namespace TodoApp.Api.Controllers
     [RoutePrefix("api/v{version:apiVersion}/items")]
     public class ItemsController : ApiController
     {
-        private static readonly Item[] Items = 
+        private readonly IItemRepository _repository;
+
+        public ItemsController(IItemRepository repository)
         {
-            new Item
-            {
-                Id = Guid.Parse("00000000-0000-0000-0000-000000000001"),
-                Text = "Learn react"
-            },
-            new Item
-            {
-                Id = Guid.Parse("00000000-0000-0000-0000-000000000002"),
-                Text = "Learn redux"
-            },
-            new Item
-            {
-                Id = Guid.Parse("00000000-0000-0000-0000-000000000003"),
-                Text = "Write Web API"
-            },
-            new Item
-            {
-                Id = Guid.Parse("00000000-0000-0000-0000-000000000004"),
-                Text = "Write dummier controller"
-            }
-        };
+            _repository = repository;
+        }
 
         // GET: api/v1/items
         [Route]
         public async Task<IHttpActionResult> GetAllItemsAsync()
-            => await Task.FromResult(Ok(Items));
+            => Ok(await _repository.GetAllAsync());
 
         // GET: api/v1/items/5
         [Route("{id}")]
         public async Task<IHttpActionResult> GetItemByIdAsync(string id)
-            => await Task.FromResult(Ok(Items[0]));
+            => Ok(await _repository.GetByIdAsync(id));
         
 
         // POST: api/v1/items
         [Route("", Name = "PostNewItem")]
         public async Task<IHttpActionResult> PostItemAsync([FromBody]Item item)
-            => await Task.FromResult(CreatedAtRoute("PostNewItem", new { id = Items[1].Id }, Items[1]));
+        {
+            await _repository.CreateAsync(item);
+            return CreatedAtRoute("PostNewItem", new { id = item.Id }, item);
+        }
 
         // PUT: api/v1/items/5
         [Route("{id}")]
         public async Task<IHttpActionResult> PutItemAsync(string id, [FromBody]Item value)
-            => await Task.FromResult(Ok(Items[2]));
+        {
+            await _repository.UpdateAsync(value);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
 
         // DELETE: api/v1/items/5
         [Route("{id}")]
         public async Task<IHttpActionResult> DeleteItemAsync(string id)
-            => await Task.FromResult(Ok(Items[3]));
+        {
+            var item = await _repository.GetByIdAsync(id);
+            if (item != null)
+            {
+                await _repository.DeleteAsync(item);
+            }
+            return Ok(item);
+        }
     }
 }
