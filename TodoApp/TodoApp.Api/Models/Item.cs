d9e2f509cfb16ad325b8b459e19d51d78ac02e6e
Peter Hutta <PeterH@kentico.com>
2018-09-11 10:25:11 +0200

    KA-367 Remove annotations, Equals and GetHashCode from Item

--- TodoApp/TodoApp.Api/Models/Item.cs
+++ TodoApp/TodoApp.Api/Models/Item.cs
@@ -8,28 +8,8 @@ namespace TodoApp.Api.Models
 {
     public class Item
     {
-        [Key]
         public string Id { get; set; }
 
-        [MinLength(1, ErrorMessage = "Text length must be greater than 1.")]
         public string Text { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            if (!(obj is Item other))
-            {
-                return false;
-            }
-
-            return Id == other.Id && Text == other.Text;
-        }
-
-        public override int GetHashCode()
-        {
-            var hashCode = -1144598946;
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Id);
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Text);
-            return hashCode;
-        }
     }
 }
