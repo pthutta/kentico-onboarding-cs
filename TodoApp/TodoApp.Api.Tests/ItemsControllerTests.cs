afc6370d8a10eef5dc8f52a47b0763bf096b6573
Peter Hutta <PeterH@kentico.com>
2018-09-11 10:41:32 +0200

    KA-367 Rewrite tests using Assert.That

--- TodoApp/TodoApp.Api.Tests/ItemsControllerTests.cs
+++ TodoApp/TodoApp.Api.Tests/ItemsControllerTests.cs
@@ -12,6 +12,7 @@ using Microsoft.Web.Http.Routing;
 using NUnit.Framework;
 using TodoApp.Api.Controllers;
 using TodoApp.Api.Models;
+using TodoApp.Api.Tests.Utils;
 
 namespace TodoApp.Api.Tests
 {
@@ -37,23 +38,23 @@ namespace TodoApp.Api.Tests
 
             var response = await _itemsController.GetAllItemsAsync();
 
-            Assert.AreEqual(expected, response);
+            Assert.That(response, Is.EquivalentTo(expected));
         }
 
         [Test]
         public async Task GetItemByIdAsync_ReturnsOkWithRequiredItem()
         {
             const string id = "1";
-            var expected = ItemsController.Items.FirstOrDefault(it => it.Id == id);
+            var expected = ItemsController.Items[0];
 
             var message = await ExecuteAsyncAction(async () => await _itemsController.GetItemByIdAsync(id));
 
             Assert.Multiple(() =>
             {
-                Assert.AreEqual(HttpStatusCode.OK, message.StatusCode);
-                Assert.IsNotNull(message.Content);
-                Assert.IsTrue(message.TryGetContentValue<Item>(out var item));
-                Assert.AreEqual(expected, item);
+                Assert.That(message.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(message.Content, Is.Not.Null);
+                Assert.That(message.TryGetContentValue<Item>(out var item), Is.Not.Null);
+                Assert.That(item.IsEqual(expected), Is.True);
             });
         }
 
@@ -61,20 +62,16 @@ namespace TodoApp.Api.Tests
         public async Task PutItemAsync_ReturnsNoContent()
         {
             const string id = "1";
+            var expected = ItemsController.Items[2];
 
-            var changedItem = new Item
-            {
-                Id = id,
-                Text = "Write tests"
-            };
-
-            var message = await ExecuteAsyncAction(async () => await _itemsController.PutItemAsync(id, changedItem));
-            var item = ItemsController.Items.FirstOrDefault(it => it.Id == id);
+            var message = await ExecuteAsyncAction(async () => await _itemsController.PutItemAsync(id, expected));
 
             Assert.Multiple(() =>
             {
-                Assert.AreEqual(HttpStatusCode.NoContent, message.StatusCode);
-                Assert.AreEqual(changedItem, item);
+                Assert.That(message.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(message.Content, Is.Not.Null);
+                Assert.That(message.TryGetContentValue<Item>(out var item), Is.Not.Null);
+                Assert.That(item.IsEqual(expected), Is.True);
             });
         }
 
@@ -82,17 +79,16 @@ namespace TodoApp.Api.Tests
         public async Task DeleteItemAsync_ReturnsOkWithDeletedItem()
         {
             const string id = "3";
-            var expected = ItemsController.Items.FirstOrDefault(it => it.Id == id);
+            var expected = ItemsController.Items[3];
 
             var message = await ExecuteAsyncAction(async () => await _itemsController.DeleteItemAsync(id));
 
             Assert.Multiple(() =>
             {
-                Assert.AreEqual(HttpStatusCode.OK, message.StatusCode);
-                Assert.IsNotNull(message.Content);
-                Assert.IsTrue(message.TryGetContentValue<Item>(out var item));
-                Assert.AreEqual(expected, item);
-                Assert.That(ItemsController.Items, Has.No.Member(item));
+                Assert.That(message.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(message.Content, Is.Not.Null);
+                Assert.That(message.TryGetContentValue<Item>(out var item), Is.Not.Null);
+                Assert.That(item.IsEqual(expected), Is.True);
             });
         }
 
@@ -109,18 +105,17 @@ namespace TodoApp.Api.Tests
                 defaults: new { id = RouteParameter.Optional }
             );
 
-            var newItem = new Item
-            {
-                Id = "4",
-                Text = "Write tests"
-            };
+            var newItem = ItemsController.Items[1];
 
             var message = await ExecuteAsyncAction(async () => await _itemsController.PostItemAsync(newItem));
 
             Assert.Multiple(() =>
             {
-                Assert.AreEqual(HttpStatusCode.Created, message.StatusCode);
-                Assert.AreEqual("http://localhost/api/test/4", message.Headers.Location.AbsoluteUri);
+                Assert.That(message.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+                Assert.That(message.Headers.Location.AbsoluteUri, Is.EqualTo("http://localhost/api/test/2"));
+                Assert.That(message.Content, Is.Not.Null);
+                Assert.That(message.TryGetContentValue<Item>(out var item), Is.Not.Null);
+                Assert.That(item.IsEqual(newItem), Is.True);
             });
         }
 
