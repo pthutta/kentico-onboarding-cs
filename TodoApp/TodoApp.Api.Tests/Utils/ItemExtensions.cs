84828574f9008fc129753d89f1ae550587c36100
Peter Hutta <PeterH@kentico.com>
2018-09-11 10:25:21 +0200

    KA-367 Add extension method for Item comparing

---
+++ TodoApp/TodoApp.Api.Tests/Utils/ItemExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Api.Models;
+
+namespace TodoApp.Api.Tests.Utils
+{
+    public static class ItemExtensions
+    {
+        public static bool IsEqual(this Item item1, Item item2)
+        {
+            return item1.Id == item2.Id && item1.Text == item2.Text;
+        }
+    }
+}
