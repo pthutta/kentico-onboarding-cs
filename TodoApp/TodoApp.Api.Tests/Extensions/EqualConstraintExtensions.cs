142bc03f00c6d8f885b77cb33994a10968069207
Peter Hutta <PeterH@kentico.com>
2018-09-13 13:23:13 +0200

    KA-368 Make ItemComparer in EqualConstraintExtensions lazy

--- TodoApp/TodoApp.Api.Tests/Extensions/EqualConstraintExtensions.cs
+++ TodoApp/TodoApp.Api.Tests/Extensions/EqualConstraintExtensions.cs
@@ -1,19 +1,20 @@
-﻿using NUnit.Framework.Constraints;
+﻿using System;
+using NUnit.Framework.Constraints;
 
 namespace TodoApp.Api.Tests.Extensions
 {
     public static class EqualConstraintExtensions
     {
-        private static readonly ItemComparer ItemComparer = new ItemComparer();
+        private static readonly Lazy<ItemComparer> ItemComparer = new Lazy<ItemComparer>();
 
         public static EqualConstraint UsingItemComparer(this EqualConstraint constraint)
         {
-            return constraint.Using(ItemComparer);
+            return constraint.Using(ItemComparer.Value);
         }
 
         public static CollectionItemsEqualConstraint UsingItemComparer(this CollectionEquivalentConstraint constraint)
         {
-            return constraint.Using(ItemComparer);
+            return constraint.Using(ItemComparer.Value);
         }
     }
 }
