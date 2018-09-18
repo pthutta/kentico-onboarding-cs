71f13eca3ccdc6954d80e9be58cca9ce0d8da89c
Peter Hutta <PeterH@kentico.com>
2018-09-18 14:01:02 +0200

    KA-368 Catch only some instances of ResolutionFailedException

--- TodoApp/Src/TodoApp.Api/Resolvers/UnityResolver.cs
+++ TodoApp/Src/TodoApp.Api/Resolvers/UnityResolver.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http.Formatting;
+using System.Web.Http.Controllers;
 using System.Web.Http.Dependencies;
+using System.Web.Http.Dispatcher;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Hosting;
+using System.Web.Http.Metadata;
+using System.Web.Http.Validation;
 using Unity;
 using Unity.Exceptions;
 
@@ -8,6 +15,19 @@ namespace TodoApp.Api.Resolvers
 {
     public class UnityResolver : IDependencyResolver
     {
+        private static readonly List<string> ExcludedTypes = new List<string>
+        {
+            typeof(IHostBufferPolicySelector).FullName,
+            typeof(IHttpControllerSelector).FullName,
+            typeof(IHttpControllerActivator).FullName,
+            typeof(IHttpActionSelector).FullName,
+            typeof(IHttpActionInvoker).FullName,
+            typeof(IContentNegotiator).FullName,
+            typeof(IExceptionHandler).FullName,
+            typeof(ModelMetadataProvider).FullName,
+            typeof(IModelValidatorCache).FullName,
+        };
+
         protected IUnityContainer Container;
 
         public UnityResolver(IUnityContainer container)
@@ -20,6 +40,7 @@ namespace TodoApp.Api.Resolvers
                 return Container.Resolve(serviceType);
             }
             catch (ResolutionFailedException)
+                when (ExcludedTypes.Contains(serviceType.FullName))
             {
                 return null;
             }
@@ -32,6 +53,7 @@ namespace TodoApp.Api.Resolvers
                 return Container.ResolveAll(serviceType);
             }
             catch (ResolutionFailedException)
+                when (ExcludedTypes.Contains(serviceType.FullName))
             {
                 return new List<object>();
             }
