074aacc1f477e08a46160d2909a5d40ece51c3e3
Peter Hutta <PeterH@kentico.com>
2018-09-19 17:24:07 +0200

    KA-368 Adjust bootstrappers according to new Api

--- TodoApp/Src/TodoApp.ApiServices/ApiServicesBootstrap.cs
+++ TodoApp/Src/TodoApp.ApiServices/ApiServicesBootstrap.cs
@@ -2,21 +2,16 @@
 using System.Web;
 using TodoApp.ApiServices.Services;
 using TodoApp.Contracts.Bootstraps;
+using TodoApp.Contracts.Containers;
 using TodoApp.Contracts.Services;
-using Unity;
-using Unity.Injection;
-using Unity.Lifetime;
 
 namespace TodoApp.ApiServices
 {
     public class ApiServicesBootstrap : IBootstrap
     {
-        public IUnityContainer RegisterTypes(IUnityContainer container) 
+        public IDependencyContainer RegisterTypes(IDependencyContainer container) 
             => container
-                .RegisterType<HttpRequestMessage>(GetHttpRequestMessageInjectionFactory())
-                .RegisterType<IUrlService, UrlService>(new HierarchicalLifetimeManager());
-
-        private static InjectionFactory GetHttpRequestMessageInjectionFactory()
-            => new InjectionFactory(_ => HttpContext.Current.Items["MS_HttpRequestMessage"] as HttpRequestMessage);
+                .RegisterType(() => HttpContext.Current.Items["MS_HttpRequestMessage"] as HttpRequestMessage)
+                .RegisterType<IUrlService, UrlService>();
     }
 }
--- TodoApp/Src/TodoApp.Database/DatabaseBootstrap.cs
+++ TodoApp/Src/TodoApp.Database/DatabaseBootstrap.cs
@@ -1,14 +1,13 @@
 ﻿using TodoApp.Contracts.Bootstraps;
+using TodoApp.Contracts.Containers;
 using TodoApp.Contracts.Repositories;
 using TodoApp.Database.Repositories;
-using Unity;
-using Unity.Lifetime;
 
 namespace TodoApp.Database
 {
     public class DatabaseBootstrap : IBootstrap
     {
-        public IUnityContainer RegisterTypes(IUnityContainer container)
-            => container.RegisterType<IItemRepository, ItemRepository>(new HierarchicalLifetimeManager());
+        public IDependencyContainer RegisterTypes(IDependencyContainer container)
+            => container.RegisterType<IItemRepository, ItemRepository>();
     }
 }
