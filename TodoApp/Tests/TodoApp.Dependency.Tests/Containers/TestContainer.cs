a8ce23e94097bfa53a85f7a462e94ed7ea979942
Peter Hutta <PeterH@kentico.com>
2018-09-20 13:56:39 +0200

    KA-368 Implement test container for dependency tests

---
+++ TodoApp/Tests/TodoApp.Dependency.Tests/Containers/TestContainer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoApp.Contracts.Bootstraps;
+using TodoApp.Contracts.Containers;
+
+namespace TodoApp.Dependency.Tests.Containers
+{
+    internal class TestContainer : IDependencyContainer
+    {
+        private readonly List<Type> _registeredTypes = new List<Type>();
+
+        public IDependencyContainer RegisterBootstrapper<TBootstrap>()
+            where TBootstrap : IBootstrap, new()
+            => new TBootstrap().RegisterTypes(this);
+
+        public IDependencyContainer RegisterType<TFrom, TTo>() where TTo : TFrom
+        {
+            _registeredTypes.Add(typeof(TFrom));
+            return this;
+        }
+
+        public IDependencyContainer RegisterType<TTo>(Func<TTo> objectGetter)
+        {
+            _registeredTypes.Add(typeof(TTo));
+            return this;
+        }
+
+        public IEnumerable<Type> GetRegisteredTypes()
+            => _registeredTypes;
+
+        public void Dispose()
+            => throw new NotImplementedException();
+
+        public object Resolve(Type type)
+            => throw new NotImplementedException();
+
+        public IEnumerable<object> ResolveAll(Type type)
+            => throw new NotImplementedException();
+
+        public IDependencyContainer CreateChildContainer()
+            => throw new NotImplementedException();
+    }
+}
